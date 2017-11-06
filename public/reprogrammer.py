7bec70cfc93e7f83b8eab941a00634ba3aa50434
Michael Fay <michaelfay95@gmail.com>
2017-11-06 00:46:51 -0600

    updated

--- public/reprogrammer.py
+++ public/reprogrammer.py
@@ -1,16 +1,28 @@
 import sys
+import ctypes
+import sys
 
-def main(ip_addr, file_location, type):
+import top_block
+
+
+
+def main(ip_addr, file_location, type, additional_args = ""):
 	response = os.system("ping -c 1 " + ip_addr)
-	
+	 
 	if(response <= 0):
 		print "Could not connect"
 		return -10
 
-	
-			
+	if sys.platform.startswith('linux'):
+		try:
+			x11 = ctypes.cdll.LoadLibrary('libX11.so')
+			x11.XInitThreads()
+		except:
+			print "Warning: failed to XInitThreads()"
 
-
+	tb = top_block_cls(uhd_args)
+	tb.Start(True)
+	tb.Wait()
 
 if __name__ == '__main__':
-	main()
+	main('192.168.0.1', '/file_location', 'n210')
